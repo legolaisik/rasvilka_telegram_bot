88bff712e426d3cbf02a1a27f952d6f7d91a9fd3
Ereduvyge <tjay.tar@gmail.com>
2023-10-14 18:12:23 +0300

    edit sql

--- sql_func.py
+++ sql_func.py
@@ -11,7 +11,6 @@ async def db_check_user(user_id, conn: sqlite3.Connection):
     except:
         return False
             
-
 # Создание юзера
 async def db_create_user(user_id, conn: sqlite3.Connection):
     try:
@@ -26,18 +25,9 @@ async def db_create_profile(user_id, conn, **kwargs):
     try:
         conn.cursor().execute()
     except:
-        return False 
+        return False     
     
-# Получение профилей для юзера
-async def db_get_user_profiles(user_id, conn: sqlite3.Connection):
-    try:
-        conn.cursor().execute("SELECT profile_name FROM profiles WHERE telegram_id = ?", (user_id))
-        return conn.cursor().fetchone()
-    except:
-        return False
-    
-    
-async def get_profile(profile_id, conn: sqlite3.Connection):
+async def db_get_profile(profile_id, conn: sqlite3.Connection):
     profile_row = conn.cursor().execute("SELECT * FROM profiles WHERE profile_id = ?", (profile_id,)).fetchone()
     if profile_row != None:
         return profile_row
